f5b7eb58998f6d9a835901757206489c19da9dac
Hendry Priyatno <hendry.priyatno@gmail.com>
2024-10-22 09:01:02 +0700

    direct login and register

--- resources/views/cms/auth/login.blade.php
+++ resources/views/cms/auth/login.blade.php
@@ -148,7 +148,7 @@
 
                     <p class="text-center">
                         <span>New on our platform?</span>
-                        <a href="#">
+                        <a href="{{route('register')}}">
                             <span>Create an account</span>
                         </a>
                     </p>
